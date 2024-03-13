425d3b6c3fdfd037faa2f1f827ba6f43609b0d89
abtsousa <11749310+abtsousa@users.noreply.github.com>
2024-03-13 09:37:02 +0000

    simpler 21

--- 21_insert_element.ml
+++ 21_insert_element.ml
@@ -1,11 +1,9 @@
-let insert_at e n l =
-  let rec aux e n l acc = match l with
-  | h :: t -> if n = 0 then acc @ e :: l else aux e (n-1) t (acc @ [h])
-  | _ -> acc @ e :: l
-  in aux e n l []
-
+let rec insert_at e n l = match l with
+| [] -> [e]
+| h :: t -> if n = 0 then e :: l else h :: insert_at e (n-1) t
 let test = insert_at "alfa" 0 ["a"; "b"; "c"; "d"];;
 let test = insert_at "alfa" 1 ["a"; "b"; "c"; "d"];;
 let test = insert_at "alfa" 2 ["a"; "b"; "c"; "d"];;
 let test = insert_at "alfa" 3 ["a"; "b"; "c"; "d"];;
 let test = insert_at "alfa" 4 ["a"; "b"; "c"; "d"];;
+let test = insert_at "alfa" 5 ["a"; "b"; "c"; "d"];;
